6b988b3b23f68ee7307f430e25c03cf0af44c8aa
RazikaBengana <razikabengana@gmail.com>
2024-04-12 05:25:30 +0200

    0. My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Display the current permissions for user 'user_0d_1' at 'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Display the current permissions for user 'user_0d_2' at 'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
