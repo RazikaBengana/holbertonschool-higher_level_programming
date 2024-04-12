d464a12bd1969c7b2484da40d7d1b5ea9100c332
RazikaBengana <razikabengana@gmail.com>
2024-04-12 02:21:10 +0200

    11. Select the best

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,9 @@
+-- Selects the score and name from the table named 'second_table'
+SELECT score, name
+FROM second_table
+
+-- Filters to include only those rows where the score is 10 or higher
+WHERE score >= 10
+
+-- Orders the results by score in descending order, from highest to lowest
+ORDER BY score DESC;
