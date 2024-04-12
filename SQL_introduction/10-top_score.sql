d0d6c99f9080906a96751a7a90bfb3609ddaaf04
RazikaBengana <razikabengana@gmail.com>
2024-04-12 02:20:57 +0200

    10. List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Selects the 'score' and 'name' columns from the 'second_table'
+-- Orders the result by 'score' in descending order, so the highest scores are listed first
+
+SELECT score, name FROM second_table ORDER BY score DESC;
