46886b75ea1752ca192c41ce03e626c28f8ae42c
RazikaBengana <razikabengana@gmail.com>
2024-04-16 19:32:25 +0200

    5. List of elements

---
+++ javascript-web_jquery/5-script.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+const $listElement = $('UL.my_list');
+const $addItemElement = $('div#add_item');
+
+$addItemElement.on('click', () => {
+  $listElement.append('<li>Item</li>');
+});
