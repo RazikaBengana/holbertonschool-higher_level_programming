3725723b35a11a566ba9fb9fc478b274295bc24e
RazikaBengana <razikabengana@gmail.com>
2024-04-16 14:52:08 +0200

    6. Loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// This script logs a series of programming language statements to the console, in 3 lines,
+// by using an array of string and a loop
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(lines[i]);
+}
